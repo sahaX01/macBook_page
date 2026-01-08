963a43bc154ba6182e8017965d6b2b184dd330fe
sahaX01 <subhamsaha.email.com>
2026-01-09 00:43:51 +0530

    Implement feature section

--- mackbook/src/components/ProductViewer.jsx
+++ mackbook/src/components/ProductViewer.jsx
@@ -17,10 +17,6 @@ function ProductViewer() {
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
       <div className="controls">
-        <p className="info">
-          Macbook Pro | Available in 14" & 16" in Space Gray and Dark colors
-        </p>
-
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
             <div
